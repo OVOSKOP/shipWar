1878f0f2db09bccac61af6e913b99ad035a636ee
al-pop <alyonka213@gmail.com>
2020-05-16 13:53:38 +0300

    COMMENT

---
+++ ShipWarClasses.hpp
@@ -0,0 +1,22 @@
+#ifndef SHIPWARCLASSES_HPP
+#define SHIRWARCLASSES_HPP
+
+class Field{
+	public:
+		Ships s[10];
+		Hit()
+};
+class Deck{
+	public: 
+		int x;
+		int y;
+};
+
+class Ships{
+	public:
+		Ships(int n);
+		int countDeck;
+		Deckd[n];
+		IsHit();
+};
+#endif //SHIPWARCLASSES_HPP
