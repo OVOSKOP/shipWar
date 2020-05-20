10532c554eec6bbbe2274c15d0bf4cbd66c8e967
OVOSKOP <parodia1234321@gmail.com>
2020-05-20 10:02:26 +0300

    Lab-14 Begin

--- Ship.cpp
+++ Ship.cpp
@@ -4,15 +4,20 @@
 
 using namespace std;
 
-Ship::Ship(int x, int y, int n, int dir){
-    Deck nextDeck;
+Ship::Ship(int n){
     deckCount = n;
-    decks.push_back(new Deck(x, y));
-    for (int i = 0; i < deckCount; i++){
-        if (dir == RIGHT) nextDeck = new Deck(x + 1, y);
+
+    decks.reserve(n);
+}
+
+bool Ship::set(int x, int y, int dir) {
+    Deck nextDeck;
+
+    for (int i = 0; i < deckCount; i++) {
+        if (dir == RIGHT) nextDeck = new Deck(x + i, y);
           else 
-            if (dir == BOTTOM) nextDeck = new Deck(x, y + 1);
-        decks.push_back(nextDeck);
+            if (dir == BOTTOM) nextDeck = new Deck(x, y + i);
+        decks.at(i) = nextDeck;
     }
 }
 
--- ShipWarClasses.hpp
+++ ShipWarClasses.hpp
@@ -6,26 +6,28 @@
 
 class Game {
 	public:
-        Player p1; //Игрок №1
-        Player p2; //Игрок №2
-        void run();// метод, который запускает игру
-        const int FIELDSIZE = 10; // number of cells = FIELDSIZE * FIELDSIZE
-        const int SHIPS_COUNT = 10;
-        const int RIGHT = 1;
-        const int BOTTOM = 0;
+	    void run();// метод, который запускает игру
+	private:
+	    Player p1; //Игрок №1
+	    Player p2; //Игрок №2
 
+	    const int FIELDSIZE = 10; // number of cells = FIELDSIZE * FIELDSIZE
+	    const int SHIPS_COUNT = 10;
+	    const int RIGHT = 1;
+	    const int BOTTOM = 0;
 };
 
 class Player {
 	public:
-        Field f; // поле игрока с его кораблями
-        // игрок ставит корабль в клетку, программа вызывает метод addShip, передает в нее количество палуб
-        // координату начальной клетки и направление, метод вызывает конструктор Ship и добавляет новый корабль в массив
-        // кораблей поля игрока.
-        vector < Ship > avalaibleShips; // инвентарь игрока с десятью кораблями
-        Player(); // конструктор игрока, который инициализирует доступные корабли
-        void rankingShip(); // расстановка кораблей игроками
-        bool isALive(); // проверяет количество оставшихся кораблей. Если кораблей > 0, то результат true
+	    Player(); // конструктор игрока, который инициализирует доступные корабли
+	    void rankingShip(); // расстановка кораблей игроками
+	    bool isALive(); // проверяет количество оставшихся кораблей. Если кораблей > 0, то результат true
+	private:
+	    Field f; // поле игрока с его кораблями
+	    // игрок ставит корабль в клетку, программа вызывает метод addShip, передает в нее количество палуб
+	    // координату начальной клетки и направление, метод вызывает конструктор Ship и добавляет новый корабль в массив
+	    // кораблей поля игрока.
+	    vector < Ship > avalaibleShips; // инвентарь игрока с десятью кораблями
 };
 
 class Field{
@@ -33,22 +35,24 @@ class Field{
 	// проверяет нахождение клетки в массиве usedDeck, если клетки нет в массиве, программа вызывает метод
 	// hit, которая засчитывает клетку в массив и если игрок попал, то засчитывает урон кораблю.
 	public:
-		vector < Ship > ships;
-        // addShip вызывает метод isFree, который проверяет свободность клетки, и если координата свободна
+	    // addShip вызывает метод isFree, который проверяет свободность клетки, и если координата свободна
 		// то в зависимости от направления прибавляет к x или y единицу и проверяет свободность 
-        // новой координаты, по количеству палуб 
-        bool addShip(int x, int y, int n, int dir);
-		vector < pair <Deck, int> > usedDeck; 
+	    // новой координаты, по количеству палуб 
+	    bool addShip(int x, int y, int n, int dir);
 		bool hit(int x, int y);
 		bool isUsed(int x, int y);
 		bool isFree(int x, int y);
-        bool isInBounds(int x, int y);
+	    bool isInBounds(int x, int y);
+	private:
+		vector < Ship > ships;
+		vector < pair <Deck, int> > usedDeck; 
 };
 
 class Deck{
 	// палуба корабля
 	public:
 		Deck(int x, int y); 
+	private:
 		int x;
 		int y;
 };
@@ -56,11 +60,12 @@ class Deck{
 class Ship{
 	public:
 		// конструктор получает количество палуб координату начальной клетки, и направление, 
-        // и создаёт корабль с этими координатами
-		Ship(int x, int y, int n, int dir);
+	    // и создаёт корабль с этими координатами
+		Ship(int n);
+		bool set(int x, int y, int dir);
 		bool isHit(int x, int y);
-        int checkSpace(int x, int y); //returns 0 if (x,y) is not in this ship
-
+	    int checkSpace(int x, int y); //returns 0 if (x,y) is not in this ship
+	private:
 		int deckCount;
 		vector < Deck > decks;
 };
